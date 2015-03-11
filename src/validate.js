d9cedaf5f0576481bbfb438364b3721d1a90f0b8
Joe Weber <wosephjeber@gmail.com>
2015-03-11 15:34:59 -0500

    Fixing issues flagged by jshint

--- src/validate.js
+++ src/validate.js
@@ -3,7 +3,7 @@ var FormValidator = function(form, options) {
   // private function to check and convert DOM object to jQuery object
   objectivize = function(obj) {
     return (obj instanceof jQuery) ? obj : $(obj);
-  }
+  };
   
   // cache this
   var _this = this;
@@ -13,41 +13,41 @@ var FormValidator = function(form, options) {
   
   // set defaults
   this.options = {
-    callback: function() { _this.processing(false); $(this).submit() },
+    callback: function() { _this.processing(false); $(this).submit(); },
     flashElement: $('.flash', _this.form),
     submitButton: $('input[type="submit"]', _this.form),
-    showFlash: function() { $(this).slideDown(500) },
-    showError: function(element, errorMessage) { $(errorMessage).slideDown(500) },
+    showFlash: function() { $(this).slideDown(500); },
+    showError: function(element, errorMessage) { $(errorMessage).slideDown(500); },
     errorClass: 'error',
     errorMessageClass: 'error-message',
     customValidations: []
-  }
+  };
   
   // extend defaults with options argument
-  var options = options || {};
+  options = options || {};
   $.extend(this.options, options);
   
   // validate form
   this.validate = function() {
     var form = _this.form;
     var errorObj = {
-          success: true,
-          errors: [],
-          messages: []
-        },
-        missingFields = 0;
+      success: true,
+      errors: [],
+      messages: []
+    },
+    missingFields = 0;
     
     // validate email address
     form.find('[type="email"]').each(function(index, element) {
       var email = $(element).val();
       if (!_this.validateEmail(email)) {
-        errorObj.messages.push('Enter valid email address. ')
+        errorObj.messages.push('Enter valid email address. ');
         errorObj.errors.push({
           element: element,
           message: 'Invalid email'
-        })
+        });
       }
-    })
+    });
     
     // validate required fields
     form.find('[required]').each(function(index, element) {
@@ -59,33 +59,32 @@ var FormValidator = function(form, options) {
         errorObj.errors.push({
           element: element,
           message: 'Required field'
-        })
+        });
       }
-    })
+    });
     
     // add custom validations
     $.each(_this.options.customValidations, function(index, validation) {
-      var validation = validation;
       var errors = false;
       if (validation.element) {
         $.each(validation.element, function(index, element) {
-          var element = validation.localScope ? _this.form.find(element) : element;
+          element = validation.localScope ? _this.form.find(element) : element;
           if (!validation.rule.call(element)) {
             errorObj.errors.push({
               element: element,
               message: validation.message
-            })
+            });
             errors = true;
           }
-        })
+        });
       } else {
         if (!validation.rule.call()) {
           errors = true;
         }
       }
       if (errors) errorObj.messages.push(validation.flash);
-    })
-
+    });
+    
     // if there are errors
     if (errorObj.errors.length > 0) {
       errorObj.success = false;
@@ -97,20 +96,20 @@ var FormValidator = function(form, options) {
     }
     
     return errorObj;
-  }
+  };
   
   // helper function to validate email with regex
   this.validateEmail = function(email) {
     var re = /^[0-9a-zA-Z][-.+_a-zA-Z0-9]*@([0-9a-zA-Z][-._0-9a-zA-Z]*\.)+[a-zA-Z]{2,6}$/;
     return re.test(email);
-  }
+  };
   
   // helper function to clear errors
   this.clearErrors = function() {
     _this.form.find('.' + _this.options.errorClass).removeClass(_this.options.errorClass);
     _this.form.find('.' + _this.options.errorMessageClass).remove();
     _this.options.flashElement.html('');
-  }
+  };
   
   // display errors
   this.displayErrors = function(errorObj) {
@@ -126,24 +125,24 @@ var FormValidator = function(form, options) {
     
     $.each(errorObj.messages, function(index, message) {
       errorMessage += message;
-    })
+    });
     
     _this.flash(errorMessage.trim());
-  }
+  };
   
   // display flash message
   this.flash = function(message) {
     _this.options.flashElement.html(message);
     
     _this.options.showFlash.call(_this.options.flashElement);
-  }
+  };
   
   this.displayFieldError = function(field, errorMessage) {
     var errorElement = $('<div class="' + _this.options.errorMessageClass + '">' + errorMessage + '</div>');
     $field = objectivize(field);
     $field.addClass(_this.options.errorClass).after(errorElement);
     _this.options.showError.call(errorMessage, errorMessage, errorElement);
-  }
+  };
   
   this.processing = function(isProcessing) {
     if (isProcessing) {
@@ -153,7 +152,7 @@ var FormValidator = function(form, options) {
       _this.options.submitButton.show();
       _this.options.submitButton.siblings('.processing-donation').hide();
     }
-  }
+  };
   
   // set up submit handler on form
   this.setUpSubmitHandler = function() {
@@ -168,8 +167,8 @@ var FormValidator = function(form, options) {
         _this.displayErrors(result);
       }
       return false;
-    })
-  }
+    });
+  };
   
   this.setUpSubmitHandler();
-}
+};
